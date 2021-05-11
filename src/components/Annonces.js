1e92ef217210c57586765b38fef69d0baf2aa316
vestias <81439787+vestias@users.noreply.github.com>
2021-05-11 15:29:03 +0000

    edit

--- src/components/Annonces.js
+++ src/components/Annonces.js
@@ -8,209 +8,234 @@ class Annonces extends React.Component {
     const annonces_data = {
       columns: [
         {
-          label: 'id',
-          field: 'id',
-          sort: 'asc',
+          label: "id",
+          field: "id",
+          sort: "asc",
           width: 150,
         },
         {
-          label: 'title',
-          field: 'title',
-          sort: 'asc',
-          width: 270
+          label: "title",
+          field: "title",
+          sort: "asc",
+          width: 270,
         },
         {
-          label: 'actions',
-          field: 'actions',
-          sort: 'asc',
-          width: 200
-        }
+          label: "actions",
+          field: "actions",
+          sort: "asc",
+          width: 200,
+        },
       ],
       rows: [
         {
-          id: '645',
-          title: 'Appartement À Vendre à Nejma Tanger',
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
+          id: "645",
+          title: "Appartement À Vendre à Nejma Tanger",
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
         },
         {
-          id: '645',
-          title: 'Appartement À Vendre à Nejma Tanger',
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
-        }, {
-          id: '645',
-          title: 'Appartement À Vendre à Nejma Tanger',
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
-        }, {
-          id: '645',
-          title: 'Appartement À Vendre à Nejma Tanger',
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
-        }, {
-          id: '645',
-          title: 'Appartement À Vendre à Nejma Tanger',
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
-        }, {
-          id: '645',
-          title: 'Appartement À Vendre à Nejma Tanger',
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
-        }, {
-          id: '645',
-          title: 'Appartement À Vendre à Nejma Tanger',
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
-        }, {
-          id: '645',
-          title: 'Appartement À Vendre à Nejma Tanger',
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
-        }, {
-          id: '645',
-          title: 'Appartement À Vendre à Nejma Tanger',
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
+          id: "645",
+          title: "Appartement À Vendre à Nejma Tanger",
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
         },
-      ]
+        {
+          id: "645",
+          title: "Appartement À Vendre à Nejma Tanger",
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
+        },
+        {
+          id: "645",
+          title: "Appartement À Vendre à Nejma Tanger",
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
+        },
+        {
+          id: "645",
+          title: "Appartement À Vendre à Nejma Tanger",
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
+        },
+        {
+          id: "645",
+          title: "Appartement À Vendre à Nejma Tanger",
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
+        },
+        {
+          id: "645",
+          title: "Appartement À Vendre à Nejma Tanger",
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
+        },
+        {
+          id: "645",
+          title: "Appartement À Vendre à Nejma Tanger",
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
+        },
+        {
+          id: "645",
+          title: "Appartement À Vendre à Nejma Tanger",
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
+        },
+      ],
     };
     return (
-      <div className="div99"> 
-            <div>
-              <h1 className="title11">List des Annonces</h1>
-              <div>
-                <MDBDataTable striped bordered small data={annonces_data} />
-              </div>{" "}
-            </div>
-          </div>
+      <div className="div99">
+        <div>
+          <h1 className="title11">List des Annonces</h1>
+          <div>
+            <MDBDataTable striped bordered small data={annonces_data} />
+          </div>{" "}
+        </div>
+      </div>
     );
   }
 }
--- src/components/slides.js
+++ src/components/slides.js
@@ -29,171 +29,198 @@ class Admin extends React.Component {
       rows: [
         {
           title: "Appartement À Vendre à Nejma Tanger",
-          image:
-          <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg</a>
-          ,
-          actions:  <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>,
+          image: (
+            <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">
+              https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg
+            </a>
+          ),
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
         },
         {
           title: "Appartement À Vendre à Nejma Tanger",
-          image:
-          <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg</a>
-          ,
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
+          image: (
+            <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">
+              https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg
+            </a>
+          ),
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
         },
         {
           title: "Appartement À Vendre à Nejma Tanger",
-          image:
-          <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg</a>
-          ,
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
+          image: (
+            <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">
+              https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg
+            </a>
+          ),
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
         },
         {
           title: "Appartement À Vendre à Nejma Tanger",
-          image:
-          <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg</a>
-          ,
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
+          image: (
+            <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">
+              https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg
+            </a>
+          ),
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
         },
         {
           title: "Appartement À Vendre à Nejma Tanger",
-          image:
-          <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg</a>
-          ,
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
+          image: (
+            <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">
+              https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg
+            </a>
+          ),
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
         },
         {
           title: "Appartement À Vendre à Nejma Tanger",
-          image:
-          <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg</a>
-          ,
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
+          image: (
+            <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">
+              https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg
+            </a>
+          ),
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
         },
         {
           title: "Appartement À Vendre à Nejma Tanger",
-          image:
-          <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg</a>
-          ,
-          actions: <div className="action_div">
-          <Link
-                 className="nav-link Modifier"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Modifier</span>
-               </Link>
-               <Link
-                 className="nav-link Supprimer"
-                 to="#"
-                 style={{ textDecoration: "none" }}
-               >
-                 <span href="#">Supprimer</span>
-               </Link>
-       </div>
-        }
+          image: (
+            <a href="https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg">
+              https://www.immobilio.ma/wp-content/uploads/2021/02/Terrain-Ain-Diab.jpeg
+            </a>
+          ),
+          actions: (
+            <div className="action_div">
+              <Link
+                className="nav-link Modifier"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Modifier</span>
+              </Link>
+              <Link
+                className="nav-link Supprimer"
+                to="#"
+                style={{ textDecoration: "none" }}
+              >
+                <span href="#">Supprimer</span>
+              </Link>
+            </div>
+          ),
+        },
       ],
     };
-    
-    
+
     return (
-      <div className="div99"> 
-            <div>
-              <h1 className="title11">Gestion des slides Logement</h1>
-              <div>
-                <MDBDataTable striped bordered small data={data} />
-              </div>{" "}
-            </div>
-          </div>
+      <div className="div99">
+        <div>
+          <h1 className="title11">Gestion des slides Logement</h1>
+          <div>
+            <MDBDataTable striped bordered small data={data} />
+          </div>{" "}
+        </div>
+      </div>
     );
   }
 }
